723da63215dca51d09a86f3443c410786a998bae
builder-247 <olaakkonen@gmail.com>
2017-08-06 22:17:33 +0300

    Updates

--- store/db.js
+++ store/db.js
@@ -4,9 +4,9 @@ const config = require("../config");
 const db =
     mongoose.connect(config.DB_URL, { useMongoClient: true }, function (err, res) {
     if (err) {
-        console.log("DB CONNECTION FAILED: " + config.DB_URL)
+        console.log("DB CONNECTION FAILED: %s", config.DB_URL)
     } else {
-        console.log("DB CONNECTED: " + config.DB_URL)
+        console.log("DB CONNECTED: %s", config.DB_URL)
     }
 });
 
---
+++ store/redis.js
@@ -0,0 +1,3 @@
+const config = require("../config");
+const redis = require("redis");
+
