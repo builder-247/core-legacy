4924c2d2fb950d801766cf56e52964754592b225
builder-247 <olaakkonen@gmail.com>
2017-08-07 18:31:20 +0300

    Moved function getBedwarsLevel from utils

--- util/Utility.js
+++ util/Utility.js
@@ -75,30 +75,10 @@ function validatePlayer(i, callback) {
 
 }
 
-function getBedwarsLevel(exp) {
-    // first few levels are different
-    if (exp < 500) {
-        return 0;
-    } else if (exp < 1500) {
-        return 1;
-    } else if (exp < 3500) {
-        return 2;
-    } else if (exp < 5500) {
-        return 3;
-    } else if (exp < 9000) {
-        return 4;
-    }
-
-    exp -= 9000;
-    return exp / 5000 + 4;
-}
-
-
 module.exports = {
     getRatio,
     hypixelFormattingIsWeird,
     removeDashes,
     colorNameToCode,
     validatePlayer,
-    getBedwarsLevel
 };
