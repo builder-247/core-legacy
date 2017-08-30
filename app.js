62ee160112e9b99a78076a6cfec1223bac543fe6
builder-247 <olaakkonen@gmail.com>
2017-08-30 13:29:23 +0300

    Added cors support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const subdomain = require('express-subdomain');
+const cors = require("cors");
 
 const db = require("./store/db");
 const routes = require('./routes/index');
@@ -15,14 +16,13 @@ const app = express();
 const router = express.Router();
 
 const APIBuilder = require("./lib/APIBuilder");
-const Hypixel = require("./HypixelAPIManager");
-const Mojang = require("./MojangAPIManager");
 const util = require("./util/Utility");
 
 // View engine set up
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hjs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,25 +32,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(subdomain('api', router));
 app.use("/", routes);
 app.use("/api", api);
-
-util.validatePlayer("hypixel", function(err, uuid) {
-    if (err) {
-        console.log("god damnit")
-    } else {
-        console.log(uuid)
-    }
-});
-
-//386258427e6a4b4980ac32d12df89791
-//ef962ec2df6e48a2ac9d6062c1b84652
-/*Hypixel("player", "&uuid=ef962ec2df6e48a2ac9d6062c1b84652", function callback(error, data) {
-    //console.log("Response: " + JSON.stringify(data));
-    APIBuilder(data);
-});*/
-
-
-//MojangAPIManager();
-
-
 
 module.exports = app;
