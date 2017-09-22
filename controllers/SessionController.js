f44f6f260a8118227c8588e5ed8c1b54389d0c02
builder_247 <olaakkonen@gmail.com>
2017-09-22 10:24:25 +0300

    Added session to API

---
+++ controllers/SessionController.js
@@ -0,0 +1,46 @@
+const util = require("../util/Utility");
+const APIBuilder = require("../lib/APIBuilder");
+const Hypixel = require("../HypixelAPIManager");
+const redis = require("../store/redis");
+
+module.exports = {
+
+    get: function (name, empty_space, callback) {
+
+        util.validatePlayer(name, isValid);
+
+        function isValid(err, uuid) {
+            if (err) {
+                callback(err, null);
+                return
+            }
+
+            redis.get("cache:session:" + uuid, function (err, cache) {
+                if (!err && cache !== null) {
+                    console.log("[CACHE] found session for %s", uuid);
+
+                    callback(null, JSON.parse(cache));
+                    return
+                }
+
+                Hypixel("session", "&uuid=" + uuid, function (error, data) {
+
+                    APIBuilder(data, uuid, null, "session", sendStats);
+
+                    function sendStats(error, response) {
+
+                        if (error) {
+                            callback(error, null);
+                            return
+                        }
+
+                        // Cache session for 10 seconds
+                        redis.setex("cache:session:" + uuid, 60, JSON.stringify(response));
+
+                        callback(null, response);
+                    }
+                });
+            });
+        }
+    }
+};
--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const PlayerController = require("./PlayerController");
 const BoosterController = require("./BoosterController");
 const GuildController = require("./GuildController");
 const FriendController = require("./FriendController");
+const SessionController = require("./SessionController");
 
 module.exports = {
     user: UserController,
     player: PlayerController,
     boosters: BoosterController,
     guild: GuildController,
-    friends: FriendController
+    friends: FriendController,
+    session: SessionController
 };
