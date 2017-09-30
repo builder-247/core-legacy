f72bf7919f382467fa4110a4a848ff7e7a800439
builder-247 <olaakkonen@gmail.com>
2017-09-30 11:08:50 +0300

    Fixed leveling bug + removed rate limiter

--- HypixelAPIManager.js
+++ HypixelAPIManager.js
@@ -1,7 +1,6 @@
 const request = require("request");
 const RateLimiter = require("limiter").RateLimiter;
 const config = require("./config");
-const limiter = new RateLimiter(config.HYPIXEL_API_LIMIT, "minute");
 const redis = require("./store/redis");
 
 const api_key = config.HYPIXEL_API_KEY;
@@ -20,29 +19,25 @@ module.exports = function (type, param, callback) {
     // session, "&uuid=" + uuid
     // friends, "&uuid=" + uuid
 
-    redis.get("API_IS_DOWN", function(err, API_IS_DOWN) {
+    redis.get("API_IS_DOWN", function (err, API_IS_DOWN) {
         if (!API_IS_DOWN) {
-            limiter.removeTokens(1, function () {
-                // console.log("https://api.hypixel.net/" + type + "?key=" + api_key + param);
-                request("https://api.hypixel.net/" + type + "?key=" + api_key + param, function (error, response, _body) {
-                    const body = JSON.parse(_body);
-                    if (error) {
-                        callback(error, null);
-                        throw error;
-                    } else if (response.statusCode !== 200) {
+            // console.log("https://api.hypixel.net/" + type + "?key=" + api_key + param);
+            request("https://api.hypixel.net/" + type + "?key=" + api_key + param, function (error, response, _body) {
+                const body = JSON.parse(_body);
+                if (error) {
+                    callback(error, null);
+                    throw error;
+                } else if (response.statusCode !== 200) {
+                    callback(response.statusCode, null)
+                } else if (body.success === false) {
+                    console.log("[Hypixel API Error]: " + body.cause);
+                    redis.setex("API_IS_DOWN", 5, true);
+                    callback(body.cause, null)
 
-                        //setTimeout(null, 5000)
+                } else {
+                    callback(null, body);
+                }
 
-                    } else if (body.success === false) {
-                        console.log("[Hypixel API Error]: " + body.cause);
-                        redis.setex("API_IS_DOWN", 5, true);
-                        callback(body.cause, null)
-
-                    } else {
-                        callback(null, body);
-                    }
-
-                })
             })
         } else {
             callback("API is down!", null);
